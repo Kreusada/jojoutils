68380cc8c910fb68e255fff4571687e1b51f74ba
Just Jojo <justjojogh@gmail.com>
2021-04-26 16:08:02 -0400

    First commit

---
+++ jojo_utils/menus.py
@@ -0,0 +1,84 @@
+"""
+MIT License
+
+Copyright (c) 2021 Jojo#7711
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from redbot.vendored.discord.ext import menus
+
+
+__all__ = ["Menu"]
+
+
+class Menu(menus.MenuPages, inherit_buttons=False):
+    """A menus class for discord
+    
+    If you would like to change the buttons subclass this!
+    """
+
+    @menus.button(
+        "\N{BLACK RIGHTWARDS ARROW}",
+        position=menus.Last(0),
+        skip_if=_skip_single_arrows,
+    )
+    async def go_to_next_page(self, payload):
+        await self.show_checked_page(self.current_page + 1)
+
+    @menus.button(
+        "\N{LEFTWARDS BLACK ARROW}",
+        position=menus.First(1),
+        skip_if=_skip_single_arrows,
+    )
+    async def go_to_previous_page(self, payload):
+        await self.show_checked_page(self.current_page - 1)
+
+    @menus.button(
+        "\N{BLACK RIGHT-POINTING DOUBLE TRIANGLE",
+        position=menus.Last(1),
+        skip_if=_skip_double_triangle_buttons,
+    )
+    async def go_to_last_page(self, payload):
+        await self.show_checked_page(self._source.get_max_pages - 1)
+
+    @menus.button(
+        "\N{BLACK LEFT-POINTING DOUBLE TRIANGLE",
+        position=menus.First(0),
+        skip_if=_skip_double_triangle_buttons,
+    )
+    async def go_to_first_page(self, payload):
+        await self.show_checked_page(0)
+
+    @menus.button("\N{CROSS MARK}")
+    async def stop_pages(self, payload):
+        self.stop()
+        await self.message.delete()
+
+    def _skip_double_triangle_buttons(self):
+        max_pages = self._source.get_max_pages()
+        if max_pages is None:
+            return True
+        return max_pages <= 4
+
+    def _skip_single_arrows(self):
+        max_pages = self._source.get_max_pages()
+        if max_pages is None:
+            return True
+        return max_pages == 1
---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r") as fp:
+    long_description = fp.read()
+
+setuptools.setup(
+    name="jojoutils",
+    version="1.0.0",
+    author="Jojo",
+    description="Tools for Jojo's cogs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Just-Jojo/jojoutils",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.8.1",
+)

630f2f6a707cbe7f280da9f178e710919c660d75
Just Jojo <justjojogh@gmail.com>
2021-04-26 16:59:10 -0400

    Release????

--- jojo_utils/__init__.py
+++ jojo_utils/__init__.py
@@ -1,5 +1,5 @@
 from .menus import *
 from .general import positive_int
 
-__version__ = "1.0.2Dev"
-verion_info = ("1", "0", "2Dev")
+__version__ = "1.0.0"
+verion_info = ("1", "0", "0")
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fp:
 
 setuptools.setup(
     name="jojoutils",
-    version="1.0.2Dev",
+    version="1.0.0",
     author="Jojo",
     description="Tools for Jojo's cogs",
     long_description=long_description,
